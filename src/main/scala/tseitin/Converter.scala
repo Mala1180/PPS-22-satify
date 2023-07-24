a51e5dc4e880ed12b2e542d2389497ba91ae540a
Alberto Paganelli <albi1600@gmail.com>
2023-07-24 12:02:04 +0200

    wip: initial implementation of Tseiting algorithm in particular substitution of variables

--- src/main/scala/tseitin/Converter.scala
+++ src/main/scala/tseitin/Converter.scala
@@ -11,14 +11,63 @@ enum Expression:
   case Clause(op: Operator)
 
 object Expression:
+  import CNFConverter.*
+  import Operator.*
+  import Expression.*
+
   def contains(exp1: Expression, exp2: Expression) : Boolean =
-    import CNFConverter.*
     subexpressions(exp1).contains(exp2)
-  def replace(exp1: Expression, exp2: Expression, l: Literal) : Expression = ???
+
+  def replace(exp1: Expression, exp2: Expression, l: Literal) : Expression =
+    exp1 match
+      case Literal(x) => literal
+      case Clause(op) == exp2 => literal
+      case Clause(op) != exp2 => subOp1(op, exp2, l)
+
+    def subExp1(exp: Expression, list: List[Expression]): List[Expression] = exp match
+      case Clause(op) => exp :: list ::: subOp1(op, list)
+      case _ => List()
+
+    def subOp1(op: Operator, exp: Operator, l: Literal): List[Expression] = op match
+      case And(exp1, exp2) => subExp1(exp1, list) ::: subExp1(exp2, list)
+      case Or(exp1, exp2) => subExp1(exp1, list) ::: subExp1(exp2, list)
+      case Xor(exp1, exp2) => subExp1(exp1, list) ::: subExp1(exp2, list)
+      case Not(exp) => subExp1(exp, list)
+
 
 trait variableGenerator:
   import Expression.*
   import Operator.*
+
+  def generateVariables(list: List[(Literal, Expression)]): List[(Literal, String)] =
+    def substitute(exp: Expression, clause: Expression, literal: Literal): String =
+      /*val prova = exp.replace(clause, literal)*/
+      val s = exp.toString.replace(clause.toString, literal.toString)
+      s
+
+    def tryes(list: List[(Literal, Expression)], res: List[(Literal, String)]): List[(Literal, String)] = list match
+      case Nil => res
+      case (literal, clause) :: tail =>
+        //println("CI ENTRO CON PRIMA ENTRY : " + literal)
+        var result: List[(Literal, String)] = List()
+        tail.foreach(subexpression => {
+          if (contains(subexpression._2, clause)) {
+            val subb = substitute(subexpression._2, clause, literal)
+            result = (subexpression._1, subb) :: res ::: result
+          }
+        })
+
+        /*        var tes: List[(Literal, Formula)] = List()
+                result.foreach(res => {
+                  tes = tail.updated(tail.indexOf((res._1, clause)), (res._1, res._2))
+                })*/
+
+        result //::: tryes(tes, result)
+
+    //tryes(tail, res ::: tryes(sub(op, List()), List()))
+    val test = list.reverse
+    println("List : " + test)
+    tryes(test, List())
 
   def printExpressions(list: List[Expression]): List[Clause] =
     list.foreach(clause => println(clause))
