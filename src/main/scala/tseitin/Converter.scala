bc93d189bec938fea862dfdf8dd785b530dcefa6
Alberto Paganelli <albi1600@gmail.com>
2023-07-24 17:55:41 +0200

    fix: initial implementation of Tseiting algorithm in particular substitution of variables: replace method

--- src/main/scala/tseitin/Converter.scala
+++ src/main/scala/tseitin/Converter.scala
@@ -21,94 +21,25 @@ object Expression:
   def replace(exp1: Expression, exp2: Expression, l: Literal): Expression = exp1 match
     case Clause(op) if Clause(op) == exp2 => l
     case Clause(And(e1, e2)) =>
-      if Clause(And(e1, e2)) == exp2 then Clause(And(replace(e1, exp2, l), replace(e1, exp2, l))) else replaceOp(And(e1,e2), exp2, l)
+      if Clause(And(e1, e2)) == exp2 then Clause(And(replace(e1, exp2, l), replace(e1, exp2, l))) else replaceOp(And(e1, e2), exp2, l)
     case Clause(Or(e1, e2)) =>
-      if Clause(Or(e1, e2)) == exp2 then Clause(Or(replace(e1, exp2, l), replace(e1, exp2, l))) else replaceOp(Or(e1,e2), exp2, l)
+      if Clause(Or(e1, e2)) == exp2 then Clause(Or(replace(e1, exp2, l), replace(e1, exp2, l))) else replaceOp(Or(e1, e2), exp2, l)
     case Clause(Not(e)) =>
       if Clause(Not(e)) == exp2 then Clause(Not(replace(e, exp2, l))) else replaceOp(Not(e), exp2, l)
-    case _ => l
-    private def replaceOp(op: Operator, exp2: Expression, l: Literal): Expression = op match
-      case And(Clause(op), c) => if Clause(op) == exp2 then Clause(And(l, c)) else Clause(And(replace(Clause(op), exp2, l), replace(c, exp2, l)))
-      case And(c, Clause(op)) => if Clause(op) == exp2 then Clause(And(c, l)) else Clause(And(replace(c, exp2, l), replace(Clause(op), exp2, l)))
-      case Or(Clause(op), c2) => if Clause(op) == exp2 then Clause(Or(l, c2)) else Clause(Or(replace(Clause(op), exp2, l), c2))
-      case Or(c1, Clause(op)) => if Clause(op) == exp2 then Clause(Or(c1, l)) else Clause(Or(c1, replace(Clause(op), exp2, l)))
-      case Not(Clause(op)) => if Clause(op) == exp2 then Clause(Not(l)) else Clause(Not(replace(Clause(op), exp2, l)))
-
-
-
- /* def subexpressions(exp: Expression): List[Expression] = exp match
-    case Literal(x) => List(exp)
-    case Clause(Not(Literal(x))) => List(exp)
-    case _ => subExp(exp, List())
-
-  private def subExp(exp: Expression, list: List[Expression]): List[Expression] = exp match
-    case Clause(op) => exp :: list ::: subOp(op, list)
-    case _ => List()
-
-  private def subOp(op: Operator, list: List[Expression]): List[Expression] = op match
-    case And(exp1, exp2) => subExp(exp1, list) ::: subExp(exp2, list)
-    case Or(exp1, exp2) => subExp(exp1, list) ::: subExp(exp2, list)
-    case Xor(exp1, exp2) => subExp(exp1, list) ::: subExp(exp2, list)
-    case Not(exp) => subExp(exp, list) */
-
-
- //FIRST TEST REPLACE
-
-/*def replace(exp1: Expression, exp2: Expression, l: Literal) : Expression =
-  exp1 match
-    case Clause(And(e1, e2)) => Clause(And(replace(e1, exp2, l), replace(e1, exp2, l)))
-    case Clause(Or(e1, e2)) => Clause(Or(replace(e1, exp2, l), replace(e1, exp2, l)))
-    case Clause(Xor(e1, e2)) => Clause(Xor(replace(e1, exp2, l), replace(e1, exp2, l)))
-    case Clause(Not(e)) => Clause(Not(replace(e, exp2, l)))
     case _ => exp1
-    //case Clause(op) != exp2 => subOp1(op, exp2, l)
-
-    def replaceOp(op: Operator, exp2: Expression, l: Literal) : Expression = op match
-      case And(Clause(op), c) if Clause(op) == exp2 => Clause(And(l, c))
-      case And(c, Clause(op)) if Clause(op) == exp2 => Clause(And(c, l))
-      case Or(Clause(op), c) if Clause(op) == exp2 => Clause(Or(l, c))
-      case Or(c, Clause(op)) if Clause(op) == exp2 => Clause(Or(c, l))
-      case Xor(Clause(op), c) if Clause(op) == exp2 => Clause(Xor(l, c))
-      case Xor(c, Clause(op)) if Clause(op) == exp2 => Clause(Xor(c, l))
-      case Not(Clause(op)) if Clause(op) == exp2 => Clause(Not(l))
-      case _ => exp2*/
-
+    private def replaceOp(op: Operator, exp2: Expression, l: Literal): Expression = op match
+      case And(Clause(op), c) => if Clause(op) == exp2 then Clause(And(l, replace(c, exp2, l))) else Clause(And(replace(Clause(op), exp2, l), replace(c, exp2, l)))
+      case And(c, Clause(op)) => if Clause(op) == exp2 then Clause(And(replace(c, exp2, l), l)) else Clause(And(replace(c, exp2, l), replace(Clause(op), exp2, l)))
+      case Or(Clause(op), c) => if Clause(op) == exp2 then Clause(Or(l, replace(c, exp2, l))) else Clause(Or(replace(Clause(op), exp2, l), replace(c, exp2, l)))
+      case Or(c, Clause(op)) => if Clause(op) == exp2 then Clause(Or(replace(c, exp2, l), l)) else Clause(Or(replace(c, exp2, l), replace(Clause(op), exp2, l)))
+      case Not(Clause(op)) => if Clause(op) == exp2 then Clause(Not(l)) else Clause(Not(replace(Clause(op), exp2, l)))
 
 
 trait variableGenerator:
   import Expression.*
   import Operator.*
 
-  def generateVariables(list: List[(Literal, Expression)]): List[(Literal, String)] =
-    def substitute(exp: Expression, clause: Expression, literal: Literal): String =
-      /*val prova = exp.replace(clause, literal)*/
-      val s = exp.toString.replace(clause.toString, literal.toString)
-      s
-
-    def tryes(list: List[(Literal, Expression)], res: List[(Literal, String)]): List[(Literal, String)] = list match
-      case Nil => res
-      case (literal, clause) :: tail =>
-        //println("CI ENTRO CON PRIMA ENTRY : " + literal)
-        var result: List[(Literal, String)] = List()
-        tail.foreach(subexpression => {
-          if (contains(subexpression._2, clause)) {
-            val subb = substitute(subexpression._2, clause, literal)
-            result = (subexpression._1, subb) :: res ::: result
-          }
-        })
-
-        /*        var tes: List[(Literal, Formula)] = List()
-                result.foreach(res => {
-                  tes = tail.updated(tail.indexOf((res._1, clause)), (res._1, res._2))
-                })*/
-
-        result //::: tryes(tes, result)
-
-    //tryes(tail, res ::: tryes(sub(op, List()), List()))
-    val test = list.reverse
-    println("List : " + test)
-    tryes(test, List())
-
+  def generateVariables(list: List[(Literal, Expression)]): List[(Literal, String)] = ???
   def printExpressions(list: List[Expression]): List[Clause] =
     list.foreach(clause => println(clause))
     List()
--- src/test/scala/TestTseitin/ReplaceTest.scala
+++ src/test/scala/TestTseitin/ReplaceTest.scala
@@ -10,7 +10,32 @@ import tseitin.CNFConverter.*
 class ReplaceTest:
 
   @Test
-  def testReplace(): Unit =
+  def testTripleReplace(): Unit =
+    // "((¬b ∧ ¬b) ∨ ¬b)" should "be replaced with ....."
+    //exp = ((¬b ∧ ¬b) ∨ ¬b)
+    //list = [X1 = ¬b, X2 = (X1 ∧ X1), X3 = X2 ∨ X1]
+    val exp: Expression = Clause(Or(Clause(And(Clause(Not(Literal("b"))), Clause(Not(Literal("b"))))), Clause(Not(Literal("b")))))
+    val substitution: Expression = Clause(Not(Literal("b")))
+    val expected: Expression = Clause(Or(Clause(And(Literal("X0"), Literal("X0"))), Literal("X0")))
+    val result = replace(exp, substitution, Literal("X0"))
+
+    println(result)
+    assertEquals(expected, result)
+
+  @Test
+  def testMultipleReplace(): Unit =
+    //exp = ((a ∧ ¬b) ∨ ¬b)
+    //list = [X1 = ¬b, X2 = (a ∧ X1), X3 = X2 ∨ X1]
+    val exp: Expression = Clause(Or(Clause(And(Literal("a"), Clause(Not(Literal("b"))))), Clause(Not(Literal("b")))))
+    val substitution: Expression = Clause(Not(Literal("b")))
+    val expected: Expression = Clause(Or(Clause(And(Literal("a"), Literal("X0"))), Literal("X0")))
+    val result = replace(exp, substitution, Literal("X0"))
+
+    println(result)
+    assertEquals(expected, result)
+
+  @Test
+  def testSimpleReplace(): Unit =
     //exp = ((a ∧ ¬b) ∨ c)
     //list = [X1 = ¬b, X2 = (a ∧ X1), X3 = X2 ∨ c]
     val exp: Expression = Clause(Or(Clause(And(Literal("a"), Clause(Not(Literal("b"))))), Literal("c")))
