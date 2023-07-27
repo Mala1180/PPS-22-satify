7b23def9e4a496e81ab2b686f00b9372c16f822f
Mattia Matteini <matteinimattia@gmail.com>
2023-07-27 16:42:05 +0200

    deps: add dependencies for achitecture testing

--- src/test/scala/TestArchitecture.scala
+++ src/test/scala/TestArchitecture.scala
@@ -1,7 +1,9 @@
-import Architecture.MVU
+import Architecture.{MVU, UpdateComponent}
 import Main.Model
-import model.{Variable, State}
-import org.scalatest.Inspectors.forAll
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.{classes, noClasses}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import update.Message
@@ -11,14 +13,53 @@ class TestArchitecture extends AnyFlatSpec with Matchers:
 
   val mvu: MVU = new MVU {}
 
-  "Model" should "be an immutable state" in {
-    mvu.model shouldBe a [State]
+  val modelClasses: JavaClasses = ClassFileImporter().importPackages("model")
+  val viewClasses: JavaClasses = ClassFileImporter().importPackages("view")
+  val updateClasses: JavaClasses = ClassFileImporter().importPackages("update")
+
+  "Model" should "be immutable" in {
+    val modelRule = classes()
+      .that()
+      .resideInAPackage("model")
+      .should()
+      .haveOnlyFinalFields
+    modelRule.check(modelClasses)
+  }
+
+  "Model" should "not depend on View and Update" in {
+    val modelRule = classes()
+      .that()
+      .resideInAPackage("model")
+      .should()
+      .haveOnlyFinalFields
+    modelRule.check(modelClasses)
+
+
+    //    val modelRule = ArchRuleDefinition.noClasses.that
+    //      .resideInAPackage(BASE_PACKAGE + ".model..")
+    //      .should
+    //      .dependOnClassesThat
+    //      .resideInAPackage(BASE_PACKAGE + ".view..")
+    //      .orShould
+    //      .dependOnClassesThat
+    //      .resideInAPackage(BASE_PACKAGE + ".update..")
+    //
+    //    modelRule.check(importedClasses)
+    //    val classes = new ClassFileImporter().importPackagesOf(classOf[UpdateComponent])
+    //
+    //    noClasses()
+    //      .that()
+    //      .haveSimpleName("SomeExample")
+    //      .should()
+    //      .dependOnClassesThat()
+    //      .haveSimpleName("Other")
+    //      .check(classes)
   }
 
   "Update" should "be a function taking a Model and a Message, returning a new Model" in {
-    mvu.update shouldBe a [(Model, Message) => Model]
+    mvu.update shouldBe a[(Model, Message) => Model]
   }
 
   "View" should "be a function which takes a Model and returns a GUI" in {
-    mvu.view shouldBe a [Model => GUI]
+    mvu.view shouldBe a[Model => GUI]
   }
