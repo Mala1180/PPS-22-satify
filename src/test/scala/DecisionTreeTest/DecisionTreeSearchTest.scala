b468fbe9c69e1823041d4ba4f580ce7cc0fe5a3f
w-disaster <luca.fabri1999@gmail.com>
2023-08-01 11:40:00 +0200

    refactor: rename dpll to decision tree search, simple test of the DecisionTree, doc: scaladoc

--- src/test/scala/DecisionTreeTest/DecisionTreeSearchTest.scala
+++ src/test/scala/DecisionTreeTest/DecisionTreeSearchTest.scala
@@ -2,16 +2,17 @@ package DecisionTreeTest
 
 import model.Expression.*
 import model.*
+import model.dpll.DecisionTreeSearch.*
 import model.dpll.DecisionTree.*
 import model.dpll.DpllExpressionUtils.*
-import model.dpll.{Decision, DecisionTree, Dpll, VarConstr}
+import model.dpll.{Decision, DecisionTree, DecisionTreeSearch, VarConstr}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.collection.immutable.Queue
 import scala.language.postfixOps
 
-class DpllTest extends AnyFlatSpec with Matchers:
+class DecisionTreeSearchTest extends AnyFlatSpec with Matchers:
 
   val varA: PartialVariable = PartialVariable("a", Option.empty)
   val varB: PartialVariable = PartialVariable("b", Option.empty)
@@ -19,18 +20,15 @@ class DpllTest extends AnyFlatSpec with Matchers:
   val parExp: PartialExpression = And(Symbol(varA), Symbol(varB))
 
   "DPLL" should "accept a Decision and return a DecisionTree" in {
-    Dpll.dpll(Decision(extractModelFromExp(parExp), parExp)).getClass should be equals classOf[DecisionTree]
+    DecisionTreeSearch.search(Decision(extractModelFromExp(parExp), parExp)).getClass should be equals classOf[DecisionTree]
   }
 
   "DPLL" should "explore all the possible assignments to the model" in {
     val emptyDec = Decision(extractModelFromExp(parExp), parExp)
-    Dpll.dpll(emptyDec) should be equals
-      // height: 0
+    search(emptyDec) should be equals
       Branch(emptyDec,
-        // height: 1
         Branch(
           Decision(Seq(PartialVariable("a", Option(true)), varB), And(Symbol(PartialVariable("a", Option(true))), Symbol(varB))),
-          // height: 2
           Branch(
             Decision(Seq(PartialVariable("a", Option(true)), PartialVariable("b", Option(true))),
               And(Symbol(PartialVariable("a", Option(true))), Symbol(PartialVariable("b", Option(true))))), Unsat, Unsat),
@@ -40,7 +38,6 @@ class DpllTest extends AnyFlatSpec with Matchers:
         ),
         Branch(
           Decision(Seq(PartialVariable("a", Option(false)), varB), And(Symbol(PartialVariable("a", Option(false))), Symbol(varB))),
-          // height: 2
           Branch(
             Decision(Seq(PartialVariable("a", Option(false)), PartialVariable("b", Option(true))),
               And(Symbol(PartialVariable("a", Option(false))), Symbol(PartialVariable("b", Option(true))))), Unsat, Unsat),
