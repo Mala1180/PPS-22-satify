51fb420d38645b6fcab98785bd304ca2cfa8ca25
w-disaster <luca.fabri1999@gmail.com>
2023-08-07 17:36:25 +0200

    chore: improve updateCnf using generics

--- src/main/scala/satify/model/dpll/DpllCnfUtils.scala
+++ src/main/scala/satify/model/dpll/DpllCnfUtils.scala
@@ -5,9 +5,10 @@ import satify.model.CNF.*
 
 object DpllCnfUtils:
 
-  /** Extract a PartialModel from an expression in CNF
-   * @param exp where to extract a Model
-   * @return Correspondent model from expression given as parameter.
+  /** Extract a PartialModel from an expression in CNF.
+   *
+   * @param cnf where to extract a Model
+   * @return Correspondent model from CNF given as parameter.
    */
   def extractModelFromCnf(cnf: CNF): PartialModel =
     cnf match
@@ -17,52 +18,15 @@ object DpllCnfUtils:
       case Not(e) => extractModelFromCnf(e)
 
   /** Update a CNF expression after a variable constraint has been applied.
-   * @param cnf to be updated
-   * @param constr constraint to be applied
-   * @return Updated PartialExpression
-   */
-  def updateCnf(cnf : CNF, constr: Constraint): CNF =
-    cnf match
-      case c: (Or | Literal) => updateCnfCommon(c, constr)
-      case a: And => updateCnfAnd(a, constr)
-
-  /**
-   * Update an Or or a Literal
    *
-   * @param cnf of type Or | Literal
-   * @param constr constraint to be applied
-   * @return Updated Or or Literal
+   * @param e      CNF subexpression to be updated
+   * @param constr constraint to apply
+   * @return Updated CNF
    */
-  private def updateCnfCommon(cnf: Or | Literal, constr: Constraint): Or | Literal =
-    cnf match
-      case Symbol(variable) => updateSymbol(Symbol(variable), constr)
-      case Or(left, right) => Or(updateCnfCommon(left, constr), updateCnfCommon(right, constr))
-      case Not(symbol) => Not(updateSymbol(symbol, constr))
-      case _ => cnf
-
-  /**
-   * Update a Symbol
-   *
-   * @param cnf of type Or | Literal
-   * @param constr constraint to be applied
-   * @return updated symbol
-   */
-  private def updateSymbol(symbol: Symbol, constr: Constraint): Symbol =
-    symbol match
+  def updateCnf[A <: CNF](e: A, constr: Constraint): A =
+    e match
       case Symbol(variable) if variable.name == constr.name =>
-        Symbol(Variable(constr.name, Option(constr.value)))
-      case _ => symbol
-
-  /**
-   * Update the right side of an And
-   *
-   * @param cnf of type
-   * @param constr constraint to be applied
-   * @return And | Or | Literal
-   */
-  private def updateCnfAnd(cnf: And | Or | Literal, constr: Constraint): And | Or | Literal =
-    cnf match
-      case Symbol(variable) => updateSymbol(Symbol(variable), constr)
-      case And(left, right) => And(updateCnfCommon(left, constr), updateCnfAnd(right, constr))
-      case Or(left, right) => updateCnfCommon(Or(left, right), constr)
-      case Not(symbol) => Not(updateSymbol(symbol, constr))
+        Symbol(Variable(constr.name, Option(constr.value))).asInstanceOf[A]
+      case Or(left, right) => Or(updateCnf(left, constr), updateCnf(right, constr)).asInstanceOf[A]
+      case Not(symbol) => Not(updateCnf(symbol, constr)).asInstanceOf[A]
+      case _ => e
