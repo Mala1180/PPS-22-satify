e4091912d1b983a8b47f6dccadb3756b357dd672
Alberto Paganelli <albi1600@gmail.com>
2023-08-08 18:05:59 +0200

    wip: complete CNF transformation with tseitin method, to do some stress test and little refactor

--- src/main/scala/satify/update/converters/TseitinTransformation.scala
+++ src/main/scala/satify/update/converters/TseitinTransformation.scala
@@ -1,6 +1,8 @@
 package satify.update.converters
 
-import satify.model.{CNF, Expression, Variable}
+import satify.model.{CNF, Expression, Literal, Variable}
+
+import scala.annotation.tailrec
 
 /** Object containing the Tseitin transformation algorithm. */
 object TseitinTransformation:
@@ -13,9 +15,17 @@ object TseitinTransformation:
     * @return the CNF expression.
     */
   def tseitin(exp: Expression): CNF =
-    val cnf = toCNF(exp)
-    // TODO to transform in CNF Model format here or in toCNF method
-    ???
+    var transformations: List[(CNFSymbol, CNF)] = List()
+    symbolsReplace(exp).foreach(s => transformations = transform(s) ::: transformations)
+    //transformations.reduce((s1, s2) => (s1._1, CNFAnd(s1._2, s2._2)))._2
+    transformations.reduceRight((s1, s2) => (s1._1, {
+      s1._2 match
+        case CNFOr(e1, e2) => s2._2 match
+          case CNFAnd(e3, e4) => CNFAnd(CNFOr(e1, e2), CNFAnd(e3, e4))
+          case CNFOr(e3, e4) => CNFAnd(CNFOr(e1, e2), CNFOr(e3, e4))
+          case _ => s2._2
+        case _ => s1._2
+    }))._2
 
   /** Substitute Symbols of nested subexpressions in all others expressions
     * @param exp the expression where to substitute Symbols
@@ -42,59 +52,24 @@ object TseitinTransformation:
     * @param exp a Symbol and the corresponding expression
     * @return a list of Symbol and expressions in CNF form for the given Symbol and expression
     */
-  import satify.model.Literal
   def transform(exp: (Expression.Symbol, Expression)): List[(CNFSymbol, CNF)] =
     def expr(exp: Expression): Literal = exp match
       case Symbol(v) => CNFSymbol(Variable(v, None))
       case Not(Symbol(v)) => CNFNot(CNFSymbol(Variable(v, None)))
       case _ => throw new Exception("Expression is not a Symbol or a Not Symbol")
-
     def symbol(exp: Expression): CNFSymbol = exp match
       case Symbol(v) => CNFSymbol(Variable(v, None))
       case _ => throw new Exception("Expression is not a Symbol")
-
     def not(exp: Expression): Literal = exp match
       case Not(Symbol(v)) => CNFSymbol(Variable(v, None))
       case Symbol(v) => CNFNot(CNFSymbol(Variable(v, None)))
       case _ => throw new Exception("Expression is not a Symbol or a Not Symbol")
-
     exp match
       case (l@Symbol(v), Not(lit)) =>
         List((symbol(Symbol(v)), CNFOr(not(lit), not(Symbol(v)))), (symbol(Symbol(v)), CNFOr(symbol(lit), symbol(Symbol(v)))))
-
       case (l@Symbol(v), And(e1, e2)) =>
         List((symbol(l), CNFOr(CNFOr(not(e1), not(e2)), symbol(l))), (symbol(l), CNFOr(expr(e1), not(l))), (symbol(l), CNFOr(expr(e2), not(l))))
-
       case (l@Symbol(v), Or(e1, e2)) =>
         List((symbol(l), CNFOr(CNFOr(expr(e1), expr(e2)), not(l))), (symbol(l), CNFOr(not(e1), symbol(l))), (symbol(l), CNFOr(not(e2), symbol(l))))
       case _ => List()
 
-    /* Transform the Symbol and the corresponding expression to CNF form
-     *
-     * @param exp a Symbol and the corresponding expression
-     * @return a list of Symbol and expressions in CNF form for the given Symbol and expression*/
-    /*def transform(exp: (Symbol, Expression)): List[(Symbol, Expression)] =
-      def not(exp: Expression): Expression = exp match
-        case Not(sym) => sym
-        case sym => Not(sym)
-
-      exp match
-        case (l, Not(lit)) => List((l, Or(Not(lit), Not(l))), (l, Or(lit, l)))
-        case (l, And(e1, e2)) =>
-          List((l, Or(Or(not(e1), not(e2)), l)), (l, Or(e1, not(l))), (l, Or(e2, not(l))))
-        case (l, Or(e1, e2)) =>
-          List((l, Or(Or(e1, e2), not(l))), (l, Or(not(e1), l)), (l, Or(not(e2), l)))
-        case _ => List()*/
-
-  /** Transforms the given expression into CNF following Tseitin Method.
-    *
-    * @param exp the expression to transform
-    * @return the expression in CNF form with new symbols introduced.
-    */
-  def toCNF(exp: Expression): Expression =
-    var transformations: List[(CNFSymbol, CNF)] = List()
-    symbolsReplace(exp).foreach(s => transformations = transform(s) ::: transformations)
-    //transformations.reduce((s1, s2) => (s1._1, CNFAnd(s1._2, s2._2)))._2
-    transformations.foreach(println)
-    ???
-    //transformations.reduceRight((s1, s2) => (s1._1, CNFAnd(s1._2, s2._2)))._2
--- src/test/scala/ExpressionUtilsTest/TseitinCNFTest.scala
+++ src/test/scala/ExpressionUtilsTest/TseitinCNFTest.scala
@@ -2,17 +2,54 @@ package ExpressionUtilsTest
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import satify.model.Expression
+import satify.model.CNF.{And as CNFAnd, Not as CNFNot, Or as CNFOr, Symbol as CNFSymbol}
 import satify.model.Expression.*
-import satify.update.converters.TseitinTransformation.{toCNF, transform}
+import satify.model.{CNF, Expression, Variable}
+import satify.update.converters.TseitinTransformation.tseitin
 
 class TseitinCNFTest extends AnyFlatSpec with Matchers:
 
   "The CNF form test" should "be" in {
     val exp = Or(And(Symbol("a"), Not(Symbol("b"))), Symbol("c"))
-    val result = toCNF(exp)
-    //result.foreach(println)
-    /*val expected = Nil
-    result shouldBe expected*/
+    val result = tseitin(exp)
+    println(result)
+    val expected = CNFAnd(
+      CNFOr(
+        CNFOr(CNFSymbol(Variable("X1", None)), CNFSymbol(Variable("c", None))),
+        CNFNot(CNFSymbol(Variable("X0", None)))
+      ),
+      CNFAnd(
+        CNFOr(CNFNot(CNFSymbol(Variable("X1", None))), CNFSymbol(Variable("X0", None))),
+        CNFAnd(
+          CNFOr(CNFNot(CNFSymbol(Variable("c", None))), CNFSymbol(Variable("X0", None))),
+          CNFAnd(
+            CNFOr(
+              CNFOr(CNFNot(CNFSymbol(Variable("a", None))), CNFNot(CNFSymbol(Variable("X2", None)))),
+              CNFSymbol(Variable("X1", None))
+            ),
+            CNFAnd(
+              CNFOr(CNFSymbol(Variable("a", None)), CNFNot(CNFSymbol(Variable("X1", None)))),
+              CNFAnd(
+                CNFOr(CNFSymbol(Variable("X2", None)), CNFNot(CNFSymbol(Variable("X1", None)))),
+                CNFAnd(
+                  CNFOr(CNFNot(CNFSymbol(Variable("b", None))), CNFNot(CNFSymbol(Variable("X2", None)))),
+                  CNFOr(CNFSymbol(Variable("b", None)), CNFSymbol(Variable("X2", None)))
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+    result shouldBe expected
   }
 
+  "The CNF form of ¬b" should "be correctly generated" in {
+    val exp = Not(Symbol("b"))
+    val result = tseitin(exp)
+    val expected: CNF = CNFAnd(
+      CNFOr(CNFNot(CNFSymbol(Variable("b", None))), CNFNot(CNFSymbol(Variable("X0", None)))),
+      CNFOr(CNFSymbol(Variable("b")), CNFSymbol(Variable("X0", None)))
+    )
+    result shouldBe expected
+  }
