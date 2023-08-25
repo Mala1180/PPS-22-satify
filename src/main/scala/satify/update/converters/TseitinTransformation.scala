4d3a3d2869fcdf1e6ac28d9eb69c62344b079979
Alberto Paganelli <albi1600@gmail.com>
2023-08-25 17:02:14 +0200

    wip: add method for Exp -> CNF conversion if allowable

--- src/main/scala/satify/update/converters/TseitinTransformation.scala
+++ src/main/scala/satify/update/converters/TseitinTransformation.scala
@@ -85,3 +85,51 @@ object TseitinTransformation:
           (symbol(s), CNFOr(not(e2), symbol(s)))
         )
       case (s @ Symbol(v), ss @ Symbol(vv)) => List((symbol(s), symbol(ss)))
+
+  /**
+   * Method to check if an expression is in CNF form and can be converted to CNF form.
+   * @param expression The expression to check.
+   * @return true if the expression could be converted to CNF form, false otherwise and in case of empty expression.
+   */
+  def isCNF(expression: Expression): Boolean =
+    import Expression.*
+    expression match
+      case Symbol(_) => true
+      case Not(Symbol(_)) => true
+
+      case And(l, r) =>
+        (l, r) match
+          case (And(_, _), _) => false
+          case _ => isCNF(l) && isCNF(r);
+
+      case Or(l, r) =>
+        (l, r) match
+          case (And(_, _), _) | (_, And(_, _)) => false
+          case _ => isCNF(l) && isCNF(r);
+      case _ => false
+
+  /**
+   * Method to check if an expression is in CNF form and can be converted to CNF form.
+   * @param expression The expression to check.
+   * @return true if the expression could be converted to CNF form, false otherwise and in case of empty expression.
+   */
+  def convertToCNF(expression: Expression): CNF =
+    def convL(exp: Expression) : CNFOr | Literal = exp match
+      case Or(l, r) => CNFOr(convL(l), convL(r))
+      case Symbol(v) => CNFSymbol(Variable(v))
+      case Not(Symbol(v)) => CNFNot(CNFSymbol(Variable(v)))
+      case _ => throw new Exception("Expression is not convertible to CNF form")
+
+    def convR(exp: Expression): CNFAnd | CNFOr | Literal = exp match
+      case And(l, r) => CNFAnd(convL(l), convR(r))
+      case Or(l, r) => CNFOr(convL(l), convL(r))
+      case Symbol(v) => CNFSymbol(Variable(v))
+      case Not(Symbol(v)) => CNFNot(CNFSymbol(Variable(v)))
+      case _ => throw new Exception("Expression is not convertible to CNF form")
+
+    expression match
+      case Symbol(v) => CNFSymbol(Variable(v))
+      case Not(Symbol(v)) => CNFNot(CNFSymbol(Variable(v)))
+      case And(l, r) => CNFAnd(convL(l), convR(r))
+      case Or(l, r) => CNFOr(convL(l), convL(r))
+      case _ => throw new Exception("Expression is not convertible to CNF form")
