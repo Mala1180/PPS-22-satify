fa7684ea818cb1c940cb29b23a1822e1390957d8
Mattia Matteini <matteinimattia@gmail.com>
2023-08-28 11:45:36 +0200

    refactor: add given conversion for tuple, modify extesion methods atMost and atLeast

--- src/test/scala/satify/dsl/ReflectionTest.scala
+++ src/test/scala/satify/dsl/ReflectionTest.scala
@@ -2,10 +2,12 @@ package satify.dsl
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.shouldBe
+import satify.model.Expression
 
 class ReflectionTest extends AnyFlatSpec:
 
   import satify.dsl.Reflection.*
+  import satify.model.Expression.*
 
   """ processInput """ should """ add "" to all variables, excluding operators """ in {
     processInput("a or not(B) and c") shouldBe """"a" or not("B") and "c""""
@@ -26,4 +28,19 @@ class ReflectionTest extends AnyFlatSpec:
   """ processInput """ should """ ignore number constants """ in {
     processInput("""atLeast(two)(a, b, c)""") shouldBe """atLeast(two)("a", "b", "c")"""
     processInput("""(a, b, c) atLeast three""") shouldBe """("a", "b", "c") atLeast three"""
+  }
+
+  """ reflection """ should """return a valid expression """ in {
+    reflect("a or not(B) and c") shouldBe And(Or(Symbol("a"), Not(Symbol("B"))), Symbol("c"))
+    reflect("(a -> b) and !c") shouldBe And(Or(Not(Symbol("a")), Symbol("b")), Not(Symbol("c")))
+    reflect("(a, b, c) atLeast two and ((c, d, e) atMost one)") shouldBe And(
+      Or(Or(And(Symbol("a"), Symbol("b")), And(Symbol("a"), Symbol("c"))), And(Symbol("b"), Symbol("c"))),
+      And(
+        And(Or(Not(Symbol("c")), Symbol("ENC0")), Or(Not(Symbol("e")), Symbol("ENC1"))),
+        And(
+          And(Or(Not(Symbol("d")), Symbol("ENC1")), Or(Not(Symbol("ENC0")), Symbol("ENC1"))),
+          Or(Not(Symbol("d")), Not(Symbol("ENC0")))
+        )
+      )
+    )
   }
