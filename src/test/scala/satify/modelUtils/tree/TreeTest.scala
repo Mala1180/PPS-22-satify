107fe43d893f212a54cfdddce3c82d61be9bb742
w-disaster <luca.fabri1999@gmail.com>
2023-08-31 16:35:18 +0200

    test: tree monad

--- src/test/scala/satify/modelUtils/tree/TreeTest.scala
+++ src/test/scala/satify/modelUtils/tree/TreeTest.scala
@@ -9,30 +9,43 @@ import satify.model.tree.TreeTraversableGiven.given_Traversable_Tree
 class TreeTest extends AnyFlatSpec with Matchers:
 
   val tree: Tree[String] = BinaryBranch(
-    "x",
+    "a",
     BinaryBranch(
-      "y",
-      UnaryBranch("a", Leaf(Some("b"))),
-      Leaf(Some("c"))
+      "b",
+      UnaryBranch("c", Leaf(Some("d"))),
+      Leaf(Some("e"))
     ),
-    Leaf(Some("z"))
+    Leaf(Some("f"))
   )
 
   "A Tree" should "be mapped to another Tree" in {
     tree.map {
-      case BinaryBranch("y", _, _) => BinaryBranch("k", Leaf(Some("a")), Leaf(Some("b")))
+      case BinaryBranch("b", _, _) => BinaryBranch("g", Leaf(Some("h")), Leaf(Some("i")))
       case node => node
-    } shouldBe BinaryBranch("x", BinaryBranch("k", Leaf(Some("a")), Leaf(Some("b"))), Leaf(Some("z")))
+    } shouldBe BinaryBranch("a", BinaryBranch("g", Leaf(Some("h")), Leaf(Some("i"))), Leaf(Some("f")))
   }
 
   "A Tree" should "be explored entirely" in {
     tree.foldLeft(List())((p: List[Tree[String]], c) => p :+ c) shouldBe
       List(
         tree,
-        BinaryBranch("y", UnaryBranch("a", Leaf(Some("b"))), Leaf(Some("c"))),
-        UnaryBranch("a", Leaf(Some("b"))),
-        Leaf(Some("b")),
-        Leaf(Some("c")),
-        Leaf(Some("z"))
+        BinaryBranch("b", UnaryBranch("c", Leaf(Some("d"))), Leaf(Some("e"))),
+        UnaryBranch("c", Leaf(Some("d"))),
+        Leaf(Some("d")),
+        Leaf(Some("e")),
+        Leaf(Some("f"))
       )
+  }
+
+  "A Tree" should "use the be mapped to another Tree using the monad" in {
+    val mTree =
+      for t <- tree
+      yield t match
+        case t if t == UnaryBranch("c", Leaf(Some("d"))) => BinaryBranch("g", Leaf(Some("h")), Leaf(Some("i")))
+        case t => t
+    mTree shouldBe BinaryBranch(
+      "a",
+      BinaryBranch("b", BinaryBranch("g", Leaf(Some("h")), Leaf(Some("i"))), Leaf(Some("e"))),
+      Leaf(Some("f"))
+    )
   }
