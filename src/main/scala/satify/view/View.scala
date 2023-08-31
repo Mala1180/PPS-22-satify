c35dac2edfcc04125d67cab024c24c483a6a1074
Alberto Paganelli <albi1600@gmail.com>
2023-08-31 18:30:32 +0200

    refactor: view update

--- src/main/scala/satify/view/View.scala
+++ src/main/scala/satify/view/View.scala
@@ -11,77 +11,32 @@ import scala.swing.*
 object View:
   def view(model: State): Set[Component] =
     import model.*
-    //Qui ho il new model.
-    //dobbiamo definire come presentare le cose e cosa far vedere e quando
-    //Allora:
-    // 1) Solution -> mostro exp e soluzione.
-    // 2) CNF -> mostro exp e CNF
-    // 3) Import -> mostro CNF e basta
-    // 4) Problem -> mostro output solo su OutputDialog inerente al problema, quindi solution e basta.
 
-
-    //print expression, cnf and solution
-    println(s"Expression: ${expression.isDefined}")
-    println(s"CNF: ${cnf.isDefined}")
-    println(s"Solution: ${solution.isDefined}")
-
-    var s : Set[Component] = Set()
-    //Solve
-    if expression.isDefined && cnf.isEmpty && solution.isDefined then
-      s = Set(updateExpression(expression), updateSolution(solution))
-
-    //CNF
-    if expression.isDefined && cnf.isDefined && solution.isEmpty then
-      s = Set(updateExpression(expression), updateCnf(cnf))
-
-    //Import
     if expression.isEmpty && cnf.isDefined && solution.isEmpty then
-      s = Set(updateExpression(cnf.get.printAsFormal()))
+      Set(updateExpression(cnf.get.printAsFormal()))
+    else updateExpression(expression) ++ updateCnf(cnf) ++ updateSolution(solution)
 
-    s
-    //Problem selection
-    //if expression.isDefined && cnf.isDefined && solution.isDefined then ???
-    //print in campo expressoin la print della cnf???
+  private def updateSolution(sol : Option[Solution]) : Set[Component] =
+    if sol.isDefined then
+      val fp: FlowPanel = new FlowPanel():
+          name = solOutputDialogName
+          contents += new BoxPanel(Orientation.Vertical):
+            contents += new ScrollPane(createOutputTextArea(sol.get.print, 30, 35))
+            contents += createNextSection()
+      Set(fp)
+    else Set()
 
+  private def updateCnf(cnf: Option[CNF]) : Set[Component] =
+    if cnf.isDefined then
+      val fp: FlowPanel = new FlowPanel():
+        name = cnfOutputDialogName
+        contents += new ScrollPane(createOutputTextArea(cnf.get.printAsFormal(), 30, 35))
+      Set(fp)
+    else Set()
 
-/*
-    val cnfComponent: FlowPanel = new FlowPanel():
-      name = cnfOutputDialogName
-      var result: String = "No CNF"
-      if model.cnf.isDefined then result = model.cnf.get.printAsFormal()
-      contents += new ScrollPane(createOutputTextArea(result, 30, 35))
-
-    val solutionComponent: FlowPanel = new FlowPanel():
-      name = solOutputDialogName
-      var result: String = "No Solution"
-      if model.solution.isDefined then result = model.solution.get.print
-      contents += new BoxPanel(Orientation.Vertical):
-        contents += new ScrollPane(createOutputTextArea(result, 30, 35))
-        contents += createNextSection()
-
-    val expComponent: TextArea = createInputTextArea(s"${if model.expression.isDefined then model.expression.get.printAsFormal(false) else ""}")
-
-    Set(expComponent, cnfComponent, solutionComponent)
-*/
-
-  private def updateSolution(sol : Option[Solution]) : Component =
-    new FlowPanel():
-      name = solOutputDialogName
-      var result: String = "No Solution"
-      if sol.isDefined then result = sol.get.print
-      contents += new BoxPanel(Orientation.Vertical):
-        contents += new ScrollPane(createOutputTextArea(result, 30, 35))
-        contents += createNextSection()
-
-  private def updateCnf(cnf: Option[CNF]) : Component =
-    new FlowPanel():
-      name = cnfOutputDialogName
-      var result: String = "No CNF"
-      if cnf.isDefined then result = cnf.get.printAsFormal()
-      contents += new ScrollPane(createOutputTextArea(result, 30, 35))
-
-  private def updateExpression(exp: Option[Expression]) : Component =
-    createInputTextArea(s"${if exp.isDefined then exp.get.printAsFormal(false) else ""}")
+  private def updateExpression(exp: Option[Expression]) : Set[Component] =
+    val ta : TextArea = createInputTextArea(s"${if exp.isDefined then exp.get.printAsFormal(false) else ""}")
+    Set(ta)
 
   private def updateExpression(exp: String): Component =
     val txt = createInputTextArea(exp)
