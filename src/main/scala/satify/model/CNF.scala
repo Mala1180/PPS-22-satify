cbcae5377aff2ad1c86d77926420d82ada5a44b7
Alberto Paganelli <albi1600@gmail.com>
2023-09-04 13:00:04 +0200

    remove comment

--- src/main/scala/satify/model/CNF.scala
+++ src/main/scala/satify/model/CNF.scala
@@ -19,26 +19,6 @@ enum CNF:
 
 object CNF:
   extension (cnf: CNF)
-    /*    def printAsFormal(flat: Boolean = false): String =
-      cnf match
-        case Symbol(value) =>
-          value match
-            case Variable(name, v) => name
-            case v => v.toString
-        case And(left, right) =>
-          if flat then s"${left.printAsFormal(flat)} ∧ ${right.printAsFormal(flat)}"
-          else s"${left.printAsFormal(flat)} ∧\n${right.printAsFormal(flat)}"
-        case Or(left, right) => s"${left.printAsFormal(flat)} ∨ ${right.printAsFormal(flat)}"
-        case Not(branch) => s"¬${branch.printAsFormal(flat)}"
-
-    def printAsDSL(flat: Boolean = false): String =
-      var r = printAsFormal(flat)
-        .replace("∧", "and")
-        .replace("∨", "or")
-        .replace("¬", "not")
-      if flat then r = r.replace("\n", " ")
-      r*/
-
     def printAsDSL(flat: Boolean = false): String =
       cnf match
         case Symbol(value) =>
