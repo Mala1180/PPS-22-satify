10358ced2824f6abf38d2daad41dc412c5493d9b
w-disaster <luca.fabri1999@gmail.com>
2023-09-10 21:00:14 +0200

    chore: dpll next sol PoC

---
+++ src/main/scala/satify/update/solver/dpll/DpllOneSol.scala
@@ -0,0 +1,62 @@
+package satify.update.solver.dpll
+
+import satify.model.CNF.*
+import satify.model.{CNF, Result, Variable}
+import satify.model.Result.*
+import satify.model.dpll.{Constraint, Decision, DecisionTree}
+import satify.model.dpll.DecisionTree.{Branch, Leaf}
+import satify.update.solver.dpll.DpllOneSol.{dpll, resume}
+import satify.update.solver.dpll.cnf.CNFSat.{isSat, isUnsat}
+import satify.update.solver.dpll.cnf.CNFSimplification.simplifyCnf
+import satify.update.solver.dpll.utils.PartialModelUtils.{extractModelFromCnf, filterUnconstrVars, updateParModel}
+
+import scala.annotation.tailrec
+import scala.util.Random
+
+object DpllOneSol:
+
+  val rnd: Random = Random(42)
+
+  def dpll(cnf: CNF): (DecisionTree, Result) = dpll(Decision(extractModelFromCnf(cnf), cnf))
+
+  def dpll(d: Decision): (DecisionTree, Result) =
+    if !isUnsat(d.cnf) && !isSat(d.cnf) then
+      randomDecisions(d) match
+        case Some((dl, dr)) =>
+          dpll(dl) match
+            case (dtl, SAT) => (Branch(d, dtl, Leaf(dr)), SAT)
+            case (dtl, UNSAT) =>
+              dpll(dr) match
+                case (dtr, res) => (Branch(d, dtl, dtr), res)
+        case None => (Leaf(d), if isSat(d.cnf) then SAT else UNSAT)
+    else (Leaf(d), if isSat(d.cnf) then SAT else UNSAT)
+
+  def resume(dt: DecisionTree): (DecisionTree, Result) = dt match
+    case Leaf(d @ Decision(_, cnf)) =>
+      val checkUnsat = isUnsat(cnf)
+      val checkSat = isSat(cnf)
+      if !checkSat && !checkUnsat then DpllOneSol.dpll(d)
+      else (dt, if checkSat then SAT else UNSAT)
+
+    case Branch(d, left, right) =>
+      resume(left) match
+        case (ldt, SAT) if ldt != left => (Branch(d, ldt, right), SAT)
+        //case (ldt, SAT) if ldt == left =>
+        case (ldt, lres) =>
+          resume(right) match
+            case (rdt, rres) => (Branch(d, ldt, rdt), if lres == SAT then SAT else rres)
+
+  private def randomDecisions(d: Decision): Option[(Decision, Decision)] = d match
+    case Decision(pm, cnf) =>
+      val fv = filterUnconstrVars(pm)
+      if fv.nonEmpty then
+        val v = rnd.nextBoolean()
+        fv(rnd.between(0, fv.size)) match
+          case Variable(n, _) =>
+            Some(
+              (
+                Decision(updateParModel(pm, Constraint(n, v)), simplifyCnf(cnf, Constraint(n, v))),
+                Decision(updateParModel(pm, Constraint(n, !v)), simplifyCnf(cnf, Constraint(n, !v)))
+              )
+            )
+      else None
---
+++ src/test/scala/satify/update/solver/dpll/DpllOneSolTest.scala
@@ -0,0 +1,103 @@
+package satify.update.solver.dpll
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import satify.model.Bool.{False, True}
+import satify.model.CNF.{And, Not, Or, Symbol}
+import satify.model.Result.*
+import satify.model.dpll.Decision
+import satify.model.dpll.DecisionTree.{Branch, Leaf}
+import satify.model.dpll.OrderedSeq.seq
+import satify.model.{CNF, Variable}
+import satify.update.solver.dpll.DpllOneSol.{dpll, resume}
+import satify.update.solver.dpll.utils.PartialModelUtils.extractModelFromCnf
+
+class DpllOneSolTest extends AnyFlatSpec with Matchers:
+
+  import satify.model.dpll.OrderedSeq.given_Ordering_Variable
+
+  val varA: Variable = Variable("a")
+  val varB: Variable = Variable("b")
+  val varC: Variable = Variable("c")
+
+  val cnf: CNF = And(Or(Symbol(varA), Symbol(varB)), Or(Symbol(varB), Symbol(varC)))
+
+  "DPLL" should "extract one solution at a time" in {
+    val firstDt = dpll(cnf)
+
+    firstDt shouldBe
+      (Branch(
+        Decision(
+          seq(varA, varB, varC),
+          And(
+            Or(Symbol(varA), Symbol(varB)),
+            Or(Symbol(varB), Symbol(varC))
+          )
+        ),
+        Branch(
+          Decision(
+            seq(Variable("a", Some(true)), varB, varC),
+            Or(Symbol(varB), Symbol(varC))
+          ),
+          Leaf(Decision(seq(Variable("a", Some(true)), Variable("b", Some(true)), varC), Symbol(True))),
+          Leaf(
+            Decision(
+              seq(Variable("a", Some(true)), Variable("b", Some(false)), varC),
+              Symbol(varC)
+            )
+          )
+        ),
+        Leaf(
+          Decision(
+            seq(Variable("a", Some(false)), varB, varC),
+            And(Symbol(varB), Or(Symbol(varB), Symbol(varC)))
+          )
+        )
+      ), SAT)
+
+    val secDt = resume(firstDt match
+      case (dt, _) => dt
+    )
+    
+    secDt shouldBe (Branch(
+      Decision(
+        seq(varA, varB, varC),
+        And(
+          Or(Symbol(varA), Symbol(varB)),
+          Or(Symbol(varB), Symbol(varC))
+        )
+      ),
+      Branch(
+        Decision(
+          seq(Variable("a", Some(true)), varB, varC),
+          Or(Symbol(varB), Symbol(varC))
+        ),
+        Leaf(Decision(seq(Variable("a", Some(true)), Variable("b", Some(true)), varC), Symbol(True))),
+        Branch(
+          Decision(
+            seq(Variable("a", Some(true)), Variable("b", Some(false)), varC),
+            Symbol(varC)
+          ),
+          Leaf(
+            Decision(
+              seq(Variable("a", Some(true)), Variable("b", Some(false)), Variable("c", Some(false))),
+              Symbol(False)
+            )
+          ),
+          Leaf(
+            Decision(
+              seq(Variable("a", Some(true)), Variable("b", Some(false)), Variable("c", Some(true))),
+              Symbol(True)
+            )
+          )
+        )
+      ),
+      Leaf(
+        Decision(
+          seq(Variable("a", Some(false)), varB, varC),
+          And(Symbol(varB), Or(Symbol(varB), Symbol(varC)))
+        )
+      )
+    ), SAT)
+
+  }
