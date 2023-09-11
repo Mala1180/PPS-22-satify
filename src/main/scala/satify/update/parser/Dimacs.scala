f9a81bfd9080e99e9abaa62db8d7d1ffc76c1f81
Alberto Paganelli <albi1600@gmail.com>
2023-09-11 18:00:23 +0200

    done: dump method for DIMACS format

--- src/main/scala/satify/update/parser/Dimacs.scala
+++ src/main/scala/satify/update/parser/Dimacs.scala
@@ -24,12 +24,13 @@ trait Dimacs[T]:
 
   private def writeSource(path: String, obj: T): Unit =
     val writer = new PrintWriter(new File(path))
-    try dump(obj).foreach(writer.println) finally writer.close()
+    try dump(obj).foreach(writer.println)
+    finally writer.close()
 
   protected def stripComments(lines: Seq[String]): Seq[String] =
     lines.filterNot(_.startsWith("c"))
 
-/** Read/write formulas in DIMACS format. */
+  /** Read/write formulas in DIMACS format. */
 object DimacsCNF extends Dimacs[CNF]:
 
   private val Header = "p cnf (\\d+) (\\d+)".r
@@ -43,98 +44,61 @@ object DimacsCNF extends Dimacs[CNF]:
         else None
       case _ => None
 
-/*  def dump(cnf: CNF): Seq[String] =
+  private def parseOr(clause: String): Option[Or | Literal] =
+    buildOrCNF(
+      clause
+        .split(" ")
+        .dropRight(1)
+        .map(s =>
+          val flatIdx = math.abs(s.toInt)
+          val literal: Literal = s.toInt match
+            case _ if s.toInt > 0 => Symbol(Variable(f"x_$flatIdx"))
+            case _ => Not(Symbol(Variable(f"x_$flatIdx")))
+          literal
+        )
+        .toSeq
+    )
 
-    val c : CNF = And(Or(Symbol(Variable("x5")), Symbol(Variable("x6"))), And(Or(Symbol(Variable("x1")), Symbol(Variable("x2"))), Or(Symbol(Variable("x3")), Symbol(Variable("x4")))))
+  private def buildOrCNF(cnf: Seq[Literal]): Option[Or | Literal] = cnf match
+    case Nil => None
+    case head +: Nil => Some(head)
+    case head +: tail =>
+      buildOrCNF(tail) match
+        case Some(value) => Some(Or(head, value))
+        case None => Some(head)
 
-
-    val header = s"p cnf ${cnf.variableCount} ${cnf.clauseCount}"
-    val clauses = cnf.clauses.map {
-      clause => s"${clause.literals.mkString(" ")} 0"
-    }
-    header +: clauses*/
+  private def buildAndCNF(cnf: Seq[Or | Literal]): Option[And | Or | Literal] = cnf match
+    case Nil => None
+    case head +: Nil => Some(head)
+    case head +: tail =>
+      buildAndCNF(tail) match
+        case Some(value) => Some(And(head, value))
+        case None => Some(head)
 
   def dump(cnf: CNF): Seq[String] =
-    var varCount = 0
     var cnfSeq: Seq[String] = Seq.empty
+    var cnt = 0
+    val memo: mutable.HashMap[CNF, String] = new mutable.HashMap[CNF, String]():
+      def nextVarName(): String =
+        cnt += 1
+        cnt.toString
 
-    var ha = new mutable.HashMap[Expression, String]() {
-      override def apply(key: CNF): String = getOrElseUpdate(key, nextVarName())
-    }
+      override def apply(key: CNF): String =
+        getOrElseUpdate(key, nextVarName())
 
-    def nextVarName(): String =
-      varCount += 1
-      varCount.toString
     import CNF.*
-    def test(cnf: CNF): String = cnf match
-      case Symbol(value) => ???
-      case And(left, right) => ???
-      case Or(left, right) => ???
-      case Not(branch) => ???
+    def dimacs(cnf: CNF): String = cnf match
+      case Symbol(value) =>
+        memo(Symbol(value))
+      case And(left, right) =>
+        dimacs(left) + "\n" + dimacs(right)
+      case Or(left, right) => dimacs(left) + " " + dimacs(right)
+      case Not(branch) =>
+        "-" + memo(branch)
 
-
-        def traverse(cnf: CNF): String = cnf match {
-          case CNF.Symbol(Variable(name, Some(value))) =>
-            s"${if (value) "" else "-"}$name"
-          case CNF.Symbol(Variable(name, None)) =>
-            nextVarName()
-          case CNF.And(left, right) =>
-            val leftStr = traverse(left)
-            val rightStr = traverse(right)
-            s"$leftStr $rightStr"
-          case CNF.Or(left, right) =>
-            val leftStr = traverse(left)
-            val rightStr = traverse(right)
-            s"$leftStr $rightStr"
-          case CNF.Not(branch) =>
-            val branchStr = traverse(branch)
-            s"-$branchStr"
-        }
-
-        val dimacsString = traverse(cnf)
-
-        // Conta il numero di variabili
-        val numVariables = varCount
-
-        // Conta il numero di clausole
-        val numClauses = dimacsString.split(" 0").length
-
-        // Genera l'intestazione DIMACS
-        cnfSeq :+= s"p $numVariables $numClauses"
-
-        // Aggiungi la rappresentazione DIMACS
-        cnfSeq ++= dimacsString.split(" 0").map(_ + " 0")
-
-        cnfSeq
-
-
-        private def parseOr(clause: String): Option[Or | Literal] =
-          buildOrCNF(
-            clause
-              .split(" ")
-              .dropRight(1)
-              .map(s =>
-                val flatIdx = math.abs(s.toInt)
-                val literal: Literal = s.toInt match
-                  case _ if s.toInt > 0 => Symbol(Variable(f"x_$flatIdx"))
-                  case _ => Not(Symbol(Variable(f"x_$flatIdx")))
-                literal
-              )
-              .toSeq
-          )
-
-        private def buildOrCNF(cnf: Seq[Literal]): Option[Or | Literal] = cnf match
-          case Nil => None
-          case head +: Nil => Some(head)
-          case head +: tail =>
-            buildOrCNF(tail) match
-              case Some(value) => Some(Or(head, value))
-              case None => Some(head)
-
-        private def buildAndCNF(cnf: Seq[Or | Literal]): Option[And | Or | Literal] = cnf match
-          case Nil => None
-          case head +: Nil => Some(head)
-          case head +: tail =>
-            buildAndCNF(tail) match
-              case Some(value) => Some(And(head, value))
-              case None => Some(head)
+    val dimacsString = dimacs(cnf)
+    val numClauses = dimacsString.split("\n").length
+    // header
+    cnfSeq :+= s"p cnf $cnt $numClauses"
+    cnfSeq ++= dimacsString.split("\n").map(_ + " 0")
+    cnfSeq
--- src/test/scala/satify/parser/DumpTest.scala
+++ src/test/scala/satify/parser/DumpTest.scala
@@ -12,37 +12,41 @@ import scala.util.Using
 
 class DumpTest extends AnyFlatSpec with Matchers {
 
-/*  "Dump method" should "dump an empty formulas" in {
-    DimacsCNF.parse(Seq("p cnf 0 0")) should matchPattern { case None => }
-  }*/
-
-  "Dump method" should "dump a simple CNF formula" in {
+  "Dump method" should "dump a simple CNF formula increasing variable values" in {
     val cnf: CNF = And(
       Or(Symbol(Variable("x")), Symbol(Variable("y"))),
       Or(Symbol(Variable("z")), Symbol(Variable("k")))
     )
-    val expected = Seq(
-      "p cnf 4 2", "1 2 0", "3 4 0"
+    val expected = List(
+      "p cnf 4 2",
+      "1 2 0",
+      "3 4 0"
     )
-
     val result = DimacsCNF.dump(cnf)
-    println(result)
+    result shouldEqual expected
+  }
 
+  "Dump method" should "dump CNF formula reusing some variable values for symbols" in {
+    val cnf: CNF =
+      And(Or(Not(Symbol(Variable("x"))), Symbol(Variable("y"))), Or(Symbol(Variable("z")), Not(Symbol(Variable("x")))))
+    val expected = List(
+      "p cnf 3 2",
+      "-1 2 0",
+      "3 -1 0"
+    )
+    val result = DimacsCNF.dump(cnf)
+    result shouldEqual expected
+  }
+
+  "Dump method" should "dump CNF formula reusing some variable values for literals" in {
+    val cnf: CNF =
+      And(Or(Symbol(Variable("x")), Symbol(Variable("y"))), Or(Symbol(Variable("z")), Not(Symbol(Variable("x")))))
+    val expected = List(
+      "p cnf 3 2",
+      "1 2 0",
+      "3 -1 0"
+    )
+    val result = DimacsCNF.dump(cnf)
     result shouldEqual expected
   }
 }
-/*  "DimacsCNF" should "open a DIMACS file and parse it" in {
-    DimacsCNF.read("src/main/resources/cnf/aim-100-1_6-no-1.txt") should matchPattern { case Some(_) => }
-  }*/
-
-  // TODO
-  /*
-  it should "dump CNF formulas" in {
-    val formula = And(
-      Or(Symbol(Variable("x_1")), Symbol(Variable("x_2"))),
-      Or(Symbol(Variable("x_3")), Not(Symbol(Variable("x_1"))))
-    )
-    DimacsCNF.dump(formula) shouldEqual Seq("p cnf 3 2", "1 2 0", "3 -1 0")
-  }
-   */
-
