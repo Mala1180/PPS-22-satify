841778398a963176eeea7f282bfd9cc4c82792ce
Alberto Paganelli <albi1600@gmail.com>
2023-09-19 16:34:27 +0200

    done: tests for GraphColoring

--- src/test/scala/satify/problems/GraphColoringTest.scala
+++ src/test/scala/satify/problems/GraphColoringTest.scala
@@ -4,22 +4,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import satify.model.Result.*
 import satify.model.Solution
-import satify.model.problems.NQueens
+import satify.model.problems.GraphColoring
 import satify.update.parser.DimacsCNF
 import satify.update.solver.SolverType.DPLL
 import satify.update.solver.{Solver, SolverType}
 
-class NQueensTest extends AnyFlatSpec with Matchers:
+class GraphColoringTest extends AnyFlatSpec with Matchers:
 
-  "NQueens 3x3" should "be UNSAT" in {
-    val problem = NQueens(3)
+  "GraphColoring 3 linked nodes with 1 color" should "be UNSAT" in {
+    // note auto-cycle
+    val problem = GraphColoring(List(("n1", "n2"), ("n2", "n3"), ("n2", "n2")), List("n1", "n2", "n3"), 2)
     val sol = Solver(DPLL).solve(problem.exp)
     sol should matchPattern { case Solution(UNSAT, _, _) => }
   }
 
-  "NQueens 4x4" should "be SAT" in {
-    val problem = NQueens(4)
+  "GraphColoring 3 linked nodes with 2 colors" should "be SAT" in {
+    val problem = GraphColoring(List(("n1", "n2"), ("n2", "n3")), List("n1", "n2", "n3"), 2)
     val sol = Solver(DPLL).solve(problem.exp)
     sol should matchPattern { case Solution(SAT, _, _) => }
-    problem.toString(sol.assignment.head)
   }
