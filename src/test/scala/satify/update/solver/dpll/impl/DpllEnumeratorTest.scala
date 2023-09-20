e7d58ce07c2cf36a2a1920a1772069936425081f
w-disaster <luca.fabri1999@gmail.com>
2023-09-20 20:41:20 +0200

    chore: decision tests, update Enumerator tests

--- src/test/scala/satify/update/solver/dpll/impl/DpllEnumeratorTest.scala
+++ src/test/scala/satify/update/solver/dpll/impl/DpllEnumeratorTest.scala
@@ -3,44 +3,25 @@ package satify.update.solver.dpll.impl
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.matchers.should.Matchers.should
-import satify.model.cnf.Bool.{False, True}
-import satify.model.cnf.CNF
 import satify.model.cnf.CNF.*
-import satify.model.dpll.DecisionTree.*
-import satify.model.dpll.OrderedList.*
-import satify.model.dpll.PartialAssignment.*
-import satify.model.dpll.{Decision, DecisionTree, OptionalVariable, PartialAssignment}
-import satify.update.solver.dpll.impl.DpllEnumerator.*
-
-import scala.annotation.tailrec
+import satify.update.solver.dpll.impl.DpllEnumerator.enumerate
+import satify.model.Result.*
+import satify.model.{Assignment, Variable}
 
 class DpllEnumeratorTest extends AnyFlatSpec with Matchers:
-
-  import satify.model.dpll.OrderedList.given
 
   val sA: Symbol = Symbol("a")
   val sB: Symbol = Symbol("b")
   val sC: Symbol = Symbol("c")
 
-  val cnf: CNF = And(sA, sB)
-
-  private def decisionFromCnf(cnf: CNF): Decision = Decision(extractParAssignmentFromCnf(cnf), cnf)
-
-  @tailrec
-  private def findLeftDecision(dt: DecisionTree, h: Int = 1): Option[Decision] = dt match
-    case Leaf(d) if h == 0 => Some(d)
-    case Branch(d, _, _) if h == 0 => Some(d)
-    case Branch(_, left, _) => findLeftDecision(left, h - 1)
-    case _ => None
-
   "Enumerator" should "return SAT" in {
-    enumerate(cnf).assignments.size should be > 0
-    enumerate(And(sA, Or(sB, sC))).assignments.size should be > 0
+    enumerate(And(sA, sB)).result shouldBe SAT
+    enumerate(And(sA, Or(sB, sC))).result shouldBe SAT
   }
 
   "Enumerator" should "return UNSAT" in {
-    enumerate(And(sA, Not(sA))).assignments should have size 0
-    enumerate(And(sA, And(Or(sB, sC), Not(sA)))).assignments should have size 0
+    enumerate(And(sA, Not(sA))).result shouldBe UNSAT
+    enumerate(And(sA, And(Or(sB, sC), Not(sA)))).result shouldBe UNSAT
     enumerate(
       And(
         Or(sA, sB),
@@ -49,56 +30,21 @@ class DpllEnumeratorTest extends AnyFlatSpec with Matchers:
           And(Or(sA, Not(sB)), Or(Not(sA), Not(sB)))
         )
       )
-    ).assignments should have size 0
+    ).result shouldBe UNSAT
   }
 
-  "Enumerator" should "do unit propagation when there's a unit literal in positive form" in {
-    val cnf: CNF = And(sA, And(sB, Or(sB, sC)))
-    findLeftDecision(dpll(decisionFromCnf(cnf))) should matchPattern {
-      case Some(
-            Decision(
-              PartialAssignment(
-                OptionalVariable("a", Some(true)) ::
-                OptionalVariable("b", None) ::
-                OptionalVariable("c", None) :: Nil
-              ),
-              _
-            )
-          ) =>
-    }
-  }
-
-  "Enumerator" should "do unit propagation when there's a unit literal in negative form" in {
-    val cnf: CNF = Or(Not(sA), Or(Not(sA), sB))
-    findLeftDecision(dpll(decisionFromCnf(cnf))) should matchPattern {
-      case Some(
-            Decision(
-              PartialAssignment(
-                OptionalVariable("a", Some(false)) ::
-                OptionalVariable("b", None) :: Nil
-              ),
-              _
-            )
-          ) =>
-    }
-  }
-
-  "Enumerator" should "do pure literals elimination when there's a pure literal in positive form" in {
-    val cnf: CNF =
-      And(
-        Or(Or(sA, Not(sB)), sB),
-        And(Or(sB, sC), Or(sA, Not(sB)))
+  "Enumerator" should "return all the assignments of a satisfiable expression" in {
+    enumerate(Or(sA, Or(sA, sB))).assignments shouldBe
+      List(
+        Assignment(List(Variable("a", true), Variable("b", true))),
+        Assignment(List(Variable("a", true), Variable("b", false))),
+        Assignment(List(Variable("a", false), Variable("b", true)))
       )
-    findLeftDecision(dpll(decisionFromCnf(cnf))) should matchPattern {
-      case Some(
-            Decision(
-              PartialAssignment(
-                OptionalVariable("a", Some(true)) ::
-                OptionalVariable("b", None) ::
-                OptionalVariable("c", None) :: Nil
-              ),
-              _
-            )
-          ) =>
-    }
+    enumerate(And(sA, Or(sA, Or(sB, Not(sC))))).assignments shouldBe
+      List(
+        Assignment(List(Variable("a", true), Variable("b", true), Variable("c", true))),
+        Assignment(List(Variable("a", true), Variable("b", false), Variable("c", true))),
+        Assignment(List(Variable("a", true), Variable("b", true), Variable("c", false))),
+        Assignment(List(Variable("a", true), Variable("b", false), Variable("c", false)))
+      )
   }
