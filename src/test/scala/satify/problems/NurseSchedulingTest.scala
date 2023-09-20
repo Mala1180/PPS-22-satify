8bf0929d66a603631825a50127a4be15e0982398
Alberto Paganelli <albi1600@gmail.com>
2023-09-20 09:48:34 +0200

    chore: nurse scheduling test names

--- src/test/scala/satify/problems/NurseSchedulingTest.scala
+++ src/test/scala/satify/problems/NurseSchedulingTest.scala
@@ -11,13 +11,13 @@ import satify.update.solver.{Solver, SolverType}
 
 class NurseSchedulingTest extends AnyFlatSpec with Matchers:
 
-  "NurseScheduling 2 nurses with 2 days and 3 shifts" should "be UNSAT" in {
+  "Scheduling of 2 nurses, 2 days and 3 shifts" should "be UNSAT" in {
     val problem = NurseScheduling(2, 2, 3)
     val sol = Solver(DPLL).solve(problem.exp)
     sol should matchPattern { case Solution(UNSAT, _, _) => }
   }
 
-  "NurseScheduling 3 nurses with 1 days and 3 shifts" should "be SAT" in {
+  "Scheduling of 3 nurses, 1 days and 3 shifts" should "be SAT" in {
     val problem = NurseScheduling(3, 1, 3)
     val sol = Solver(DPLL).solve(problem.exp)
     sol should matchPattern { case Solution(SAT, _, _) => }
