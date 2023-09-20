b2d726b8a3d6779c1dd0763fa05b6a9b01735d38
w-disaster <luca.fabri1999@gmail.com>
2023-09-20 10:43:35 +0200

    undo NQueens 10x10

--- src/test/scala/satify/problems/NQueensTest.scala
+++ src/test/scala/satify/problems/NQueensTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.should.Matchers
 import satify.model.Result.*
 import satify.model.Solution
 import satify.model.problems.NQueens
-import satify.model.problems.NQueens.printNQueensFromDimacs
 import satify.update.parser.DimacsCNF
 import satify.update.solver.SolverType.DPLL
 import satify.update.solver.{Solver, SolverType}
@@ -22,26 +21,5 @@ class NQueensTest extends AnyFlatSpec with Matchers:
     val problem = NQueens(4)
     val sol = Solver(DPLL).solve(problem.exp)
     sol should matchPattern { case Solution(SAT, _, _) => }
-    println("NQueens 4x4")
-    problem.printNQueens(sol.assignments.head)
+    problem.toString(sol.assignments.head)
   }
-
-  "NQueens 10x10" should "be SAT" in {
-    val oCnf = DimacsCNF.read("src/main/resources/cnf/nqueens10.txt")
-    oCnf should matchPattern { case Some(_) => }
-    val sol = Solver(DPLL).solve(oCnf.get)
-    sol should matchPattern { case Solution(SAT, _, _) => }
-    println("NQueens 10x10")
-    printNQueensFromDimacs(10, sol.assignments.head)
-  }
-
-/*
-  "NQueens 15x15" should "be SAT" in {
-    val oCnf = DimacsCNF.read("src/main/resources/cnf/nqueens20.txt")
-    oCnf should matchPattern { case Some(_) => }
-    val sol = Solver(DPLL).solve(oCnf.get)
-    sol should matchPattern { case Solution(SAT, _) => }
-    println("NQueens 15x15")
-    printNQueensFromDimacs(15, sol.assignment.head)
-  }
- */
