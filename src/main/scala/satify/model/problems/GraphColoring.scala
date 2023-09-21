11ce067fab534959395fa6aaf7e167ef81c5aa9f
Mattia Matteini <matteinimattia@gmail.com>
2023-09-21 16:14:45 +0200

    chore: add check of graph coloring input

--- src/main/scala/satify/model/problems/GraphColoring.scala
+++ src/main/scala/satify/model/problems/GraphColoring.scala
@@ -4,12 +4,14 @@ import satify.model.expression.Expression
 import satify.model.expression.Expression.*
 import satify.model.expression.SymbolGeneration.{SymbolGenerator, encodingVarPrefix}
 
+
 case class GraphColoring(edges: List[(String, String)], nodes: List[String], colors: Int) extends Problem:
 
   given SymbolGenerator with
     def prefix: String = encodingVarPrefix
 
   val variables: Seq[Seq[Symbol]] =
+    edges.foreach(e => require(nodes.contains(e._1) && nodes.contains(e._2)))
     for i <- nodes.indices yield for j <- 0 until colors yield Symbol(s"${nodes(i)}_c$j")
 
   /** Each node has exactly one color */
